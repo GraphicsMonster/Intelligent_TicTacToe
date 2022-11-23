fadfa71048c010c8d1c68d3e055624ae40cd1d8a
Devansh Gupta <63590510+GraphicsMonster@users.noreply.github.com>
2022-11-23 19:40:49 +0530

    First commit
    Added the necessary files.

---
+++ tictactoe.py
@@ -0,0 +1,194 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import numpy as npy
+import sys
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if board == initial_state():
+        return X
+
+    numpy_board = npy.array(board)
+
+    Xno = npy.count_nonzero(numpy_board == X)
+
+    Ono = npy.count_nonzero(numpy_board == O)
+
+    if Xno > Ono:
+
+        return O
+
+    else:
+
+        return X
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    Result = set()
+
+    for k in range(3):
+
+        for l in range(3):
+
+            if board[k][l] == EMPTY:
+
+                Result.add((k, l))
+
+    return Result
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    i = action[0]
+    j = action[1]
+
+    if board[i][j] != EMPTY:
+        raise Exception("Invalid Action")
+
+    new_player = player(board)
+
+    new_board = copy.deepcopy(board)
+
+    new_board[i][j] = new_player
+
+    return new_board
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    for i in range(3):
+
+        if (board[i][0] == board[i][1] == board[i][2] and board[i][0] != EMPTY):
+
+            return board[i][0]
+
+        if (board[0][0] == board[1][1] == board[2][2] or (board[0][2] == board[1][1] == board[2][0]) and board[1][1] != EMPTY):
+
+             return board[1][1]
+        if (board[0][i] == board[1][i] == board[2][i] and board[0][i] != EMPTY):
+
+             return board[1][i]
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    if winner(board) != None:
+
+        return True;
+
+    numpy_board = npy.array(board)
+
+    empty_no = npy.count_nonzero(numpy_board == EMPTY)
+
+    if (empty_no == 0):
+
+        return True
+    else:
+
+        return False
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    win_player = winner(board)
+
+    if (win_player == X):
+
+        return 1
+
+    elif (win_player == O):
+
+        return -1
+
+    else:
+
+        return 0
+
+
+def minimax(board):
+    current_player = player(board)
+
+    if current_player == X:
+
+        value = -math.inf
+
+        for action in actions(board):
+
+            k = min_value(result(board, action))
+
+            if k > value:
+
+                value = k
+
+                optimalAction = action
+    else:
+        value = math.inf
+
+        for action in actions(board):
+
+            k = max_value(result(board, action))
+
+            if k < value:
+
+                value = k
+
+                optimalAction = action
+
+    return optimalAction
+
+def max_value(board):
+
+    if terminal(board):
+
+        return utility(board)
+
+    value = -math.inf
+
+    for action in actions(board):
+
+        value = max(value, min_value(result(board, action)))
+
+    return value
+
+
+def min_value(board):
+
+    if terminal(board):
+
+        return utility(board)
+
+    value = math.inf
+
+    for action in actions(board):
+
+        value = min(value, max_value(result(board, action)))
+
+    return value
